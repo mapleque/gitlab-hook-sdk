6d5e74c2b7c4905dbc370bcd5ee59c772c9e1833
mapleque <mapleque@163.com>
2023-01-18 19:42:28 +0800

    feat: parse event payload

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Gitlab Hook SDK
 ====
 
+How To Use
+----
+
+See example file: [example_serve_test.go](webhook/eample_serve_test.go)
+Or [![godoc reference](https://godoc.org/github.com/mapleque/gitlab-hook-sdk/webhook?status.svg)](http://pkg.go.dev/github.com/mapleque/gitlab-hook-sdk/webhook)
---
+++ example_serve_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/mapleque/gitlab-hook-sdk/webhook"
+)
+
+func Example_serve() {
+	var opts []webhook.Option
+	secret := os.Getenv("GITLAB_WEBHOOK_SECRET")
+	if secret != "" {
+		opts = append(opts, webhook.Options.Secret(secret))
+	}
+	opts = append(
+		opts,
+		webhook.Handlers.Default(func(payload webhook.Payload) {
+			log.Printf("recieve webhook hook payload: %v", payload)
+		}),
+		webhook.Handlers.IssueEvent(func(payload webhook.IssueEventPayload) {
+			log.Printf("here is an issue event payload: %v", payload)
+		}),
+	)
+	hook, _ := webhook.New(opts...)
+
+	http.Handle("/mywebhook", hook)
+
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+		log.Fatalf("webhook hook serve failed with error: %v", err)
+	}
+}
